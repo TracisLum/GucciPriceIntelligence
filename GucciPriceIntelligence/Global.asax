54f1db4c2110ca968ef97eda99107951db371876
TracisLum <tracislum@gmail.com>
2018-04-01 16:54:14 +1200

    添加项目文件。

---
+++ GucciPriceIntelligence/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GucciPriceIntelligence.MvcApplication" Language="C#" %>
---
+++ GucciPriceIntelligence/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GucciPriceIntelligence.Startup))]
+namespace GucciPriceIntelligence
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
