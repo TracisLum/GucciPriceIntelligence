5d2fdd6186e812131da64a7f0d03652b95b906a9
TracisLum <tracislum@gmail.com>
2018-04-02 01:28:29 +1200

    Made some changes

---
+++ GucciPriceIntelligence/Utilities/CountryHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using GucciPriceIntelligence.Utilities.Db;
+
+namespace GucciPriceIntelligence.Utilities
+{
+    public static class CountryHelper
+    {
+        public const string TableName = "Country";
+        public const string LastUpdateFieldName = "LastUpdate";
+
+        //Update exchange rate
+        public static bool UpdateExchangeRate()
+        {
+            DbHelper dbHelper = new DbHelper();
+            SqlConnection conn = dbHelper.connection;
+            SqlCommand cmd = conn.CreateCommand();
+
+
+
+            return dbHelper.CloseConnection();
+        }
+
+        //Check is exchange rate updated today or not
+        public static bool IsExchangeRateUpdated(string code)
+        {
+            DbHelper dbHelper = new DbHelper();
+            SqlConnection conn = dbHelper.connection;
+            SqlCommand cmd = conn.CreateCommand();
+
+            string curTimeStr = DateTime.Now.ToString("yyyy-MM-dd-");
+            //Select CONVERT(varchar(100), GETDATE(), 23)
+            cmd.CommandText = "select CONVERT(varchar(10), @lastupdate, 23) from " + TableName + "where code = @code";
+            cmd.Parameters.Add("@lastupdate", SqlDbType.VarChar);
+            cmd.Parameters["@lastupdate"].Value = LastUpdateFieldName;
+            cmd.Parameters.Add("@code", SqlDbType.VarChar);
+            cmd.Parameters["@code"].Value = code;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                if (reader.GetString(0).Equals(curTimeStr)) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ GucciPriceIntelligence/Utilities/Db/DBHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GucciPriceIntelligence.Utilities.Db
+{
+    public class DbHelper
+    {
+        public SqlConnection connection;
+
+        public DbHelper()
+        {
+            OpenConnection();
+        }
+
+        //Open _connection
+        public bool OpenConnection()
+        {
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            try
+            {
+                if (connection.State.ToString() != "Open")
+                {
+                    connection.Open();
+                }
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+
+        //Close _connection
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        
+    }
+}
