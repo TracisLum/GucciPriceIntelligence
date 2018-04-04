d948eda80fe9eab6f37bafb54a75b16033d0175c
TracisLum <tracislum@gmail.com>
2018-04-04 23:46:04 +1200

    Make banner image parallax.

--- GucciPriceIntelligence/Models/Entities/ProductCategory.cs
+++ GucciPriceIntelligence/Models/Entities/ProductCategory.cs
@@ -7,8 +7,7 @@ namespace GucciPriceIntelligence.Models.Entities
 {
     public class ProductCategory
     {
-        public int Id { get; set; }
         public string Name { get; set; }
-        public string Tag_1 { get; set; }
+        public int Level { get; set; }
     }
 }
---
+++ GucciPriceIntelligence/Utilities/Db/CategoryHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using GucciPriceIntelligence.Models.Entities;
+
+namespace GucciPriceIntelligence.Utilities.Db
+{
+    public static class CategoryHelper
+    {
+        public static List<ProductCategory> GetCategories(int level)
+        {
+
+            return null;
+        }
+
+    }
+}
