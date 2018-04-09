2c41f8e242da6e70c35dbafc65181a48378da994
TracisLum <tracislum@gmail.com>
2018-04-09 21:29:45 +1200

    Fix db issues

--- GucciPriceIntelligence/Models/Classes/Product.cs
+++ GucciPriceIntelligence/Models/Classes/Product.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using GucciPriceIntelligence.Models.Classes;
 
 namespace GucciPriceIntelligence.Models.Entities
 {
-    public class Product : BaseModelClass
+    public class Product
     {
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public float ProductPrice { get; set; }
+        [Key]
+        public string Code { get; set; }
+        public string Name { get; set; }
         public int CategoryId { get; set; }
-        public int CountryId { get; set; }
+        public double USD { get; set; }
+        public double CNY { get; set; }
     }
 }
